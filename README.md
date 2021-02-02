437037eb930add82dd3e74c8e60535acd8de85e8
janusshi <janusshi@tencent.com>
2021-02-02 16:42:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+一个可以在nuxt编译阶段通过解析代码中的注释，分割服务端或者客户端代码的工具
+A tool that can split the server or client code by parsing the comments in the code during the nuxt compilation stage
+安装
+```
+tnpm i nuxt-server-client-diff-loader
+```
+
+使用
+
+in the code
+```
+// client js begin
+window...
+// client js end
+
+// server js begin
+global...
+// server js end
+
+```
+nuxt.config.js
+```
+build:{
+  extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.ts$|\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'nuxt-server-client-diff-loader',
+          options: {
+            isServer: ctx.isServer,
+          },
+        },
+      });
+    },
+}
+```
---
+++ loader.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var loaderUtils = require('loader-utils');
+
+/* eslint-disable no-param-reassign */
+module.exports = function (content) {
+  const options = loaderUtils.getOptions(this);
+  const loaderReg = {
+    server: {
+      beginReg: /\/\/\s*(only\s*)?client\s*js\s*begin/,
+      endReg: /\/\/\s*(only\s*)?client\s*js\s*end/,
+    },
+    client: {
+      beginReg: /\/\/\s*(only\s*)?server\s*js\s*begin/,
+      endReg: /\/\/\s*(only\s*)?server\s*js\s*end/,
+    },
+  };
+  return splitOnlyLoader(content, options.isServer ? loaderReg.server : loaderReg.client);
+};
+
+// client js begin
+// client js end
+// server js begin
+// server js end
+
+function splitOnlyLoader(content, { beginReg, endReg }) {
+  let finish = false;
+  let tempContent = '';
+
+  while (!finish) {
+    const begin = content.search(beginReg);
+    const end = content.search(endReg);
+
+    if (begin === -1 || end === -1) break;
+
+    if (begin > end) {
+      this.emitError(new Error('js end comments in front of begin comments'));
+    } else if (end > begin) {
+      tempContent += content.slice(0, begin);
+      content = content.slice(end + content.match(endReg)[0].length);
+    } else {
+      finish = true;
+    }
+  }
+
+  tempContent += content;
+
+  return tempContent;
+}
